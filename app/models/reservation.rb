c7d833cc129dd330fd7508a20ed967e106a5be6c
Andy Tran <andy8tran@gmail.com>
2017-09-12 17:35:25 -0400

    added reservation validation for party size

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,20 +1,18 @@
 class Reservation < ApplicationRecord
   belongs_to :seating
 
-  delegate :restaurant, to: :seating, allow_nil: false
-
-  validate :seating_full, :party_too_large
+  validate   :seating_full
 
   def seating_full
-    if seating.remaining_seats + party_size > restaurant.capacity
-      errors.add(:seating, "doesn't have enough seats! There are only #{seating.remaining_seats} available.")
+    if party_size > self.seating.remaining_seats
+      errors.add(:time_slot, "doesn't have enough seats! There are only #{seating.remaining_seats} available.")
     end
   end
-
-  def party_too_large
-    if party_size > restaurant.capacity
-      errors[:base] << "Your party size is too big for this restaurant!"
-    end
-  end
+  #
+  # def party_too_large
+  #   if party_size > self.seating.restaurant.capacity
+  #     errors[:base] << "Your party size is greater than this restaurant's capacity!!"
+  #   end
+  # end
 
 end
