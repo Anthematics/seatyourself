1c58bd8db129c9aa20022e1b0e82d10417c536b1
Andy Tran <andy8tran@gmail.com>
2017-09-12 15:46:44 -0400

    changed schema

--- db/migrate/20170912173920_add_owner_to_restaurant.rb
+++ db/migrate/20170912173920_add_owner_to_restaurant.rb
@@ -1,5 +1,5 @@
 class AddOwnerToRestaurant < ActiveRecord::Migration[5.0]
   def change
-    add_reference :Restaurants, :Owner, index: true, foreign_key:true
+    add_reference :restaurants, :owner, index: true, foreign_key:true
   end
 end
