0aadafe371a5968a6d31b4a9d356ed44b6618a59
Andy Tran <andy8tran@gmail.com>
2017-09-13 18:48:48 -0400

    added description column to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913192923) do
+ActiveRecord::Schema.define(version: 20170913224516) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "seating_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170913192923) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.text     "description"
   end
 
   create_table "seatings", force: :cascade do |t|
